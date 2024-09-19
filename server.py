1ce520ee2195409abeb8d2e27c10eb87eb837160
Maziar Sojoudian <maziar@sojoudian.net>
2024-09-18 21:59:14 -0400

    fix image and add html

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import os
+import mimetypes
 
 class myHandler(BaseHTTPRequestHandler):
     def do_GET(self):
@@ -7,12 +8,19 @@ class myHandler(BaseHTTPRequestHandler):
             self.path = "/index.html"
 
         try:
-            # Read the requested file from the current directory
-            file_to_open = open(self.path[1:]).read()
-            self.send_response(200)
-            self.send_header('Content-Type', 'text/html')
-            self.end_headers()
-            self.wfile.write(file_to_open.encode('utf-8'))
+            # Get the full file path
+            file_path = self.path[1:]  # remove the leading "/"
+            
+            # Determine the MIME type of the file
+            mime_type, _ = mimetypes.guess_type(file_path)
+            
+            # Open and read the requested file
+            with open(file_path, 'rb') as file:
+                self.send_response(200)
+                self.send_header('Content-Type', mime_type)
+                self.end_headers()
+                self.wfile.write(file.read())
+        
         except Exception as e:
             self.send_response(404)
             self.send_header('Content-Type', 'text/plain')
