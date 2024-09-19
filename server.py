5af92325e9dd72f4e2755821a9d323c91516b992
Maziar Sojoudian <maziar@sojoudian.net>
2024-09-18 21:52:21 -0400

    update server to servHTML

--- server.py
+++ server.py
@@ -1,15 +1,27 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
+import os
 
 class myHandler(BaseHTTPRequestHandler):
     def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-Type', 'text/plain')
-        self.end_headers()
-        self.wfile.write(b"Hello MST 300!")
+        if self.path == "/":
+            self.path = "/index.html"
 
-def run (server_class=HTTPServer, hanler_class=myHandler, port=80):
+        try:
+            # Read the requested file from the current directory
+            file_to_open = open(self.path[1:]).read()
+            self.send_response(200)
+            self.send_header('Content-Type', 'text/html')
+            self.end_headers()
+            self.wfile.write(file_to_open.encode('utf-8'))
+        except Exception as e:
+            self.send_response(404)
+            self.send_header('Content-Type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(b"404 Not Found")
+
+def run(server_class=HTTPServer, handler_class=myHandler, port=80):
     server_address = ('', port)
-    httpd = server_class(server_address, hanler_class)
+    httpd = server_class(server_address, handler_class)
     print(f'Server running on port {port}')
     httpd.serve_forever()
 
